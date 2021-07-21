3d77a00574d21aca789e0a56a74f4fcfaddd30be
Jesús Ramírez <jesus@ramirezduval.com>
2021-07-21 16:48:19 +0200

    Init next page

--- pages/index.js
+++ pages/index.js
@@ -21,13 +21,13 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://kavehome.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          <span>Powered by{' '}</span>
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <Image src="/kavehome.svg" alt="kavehome" width={120} height={24} />
           </span>
         </a>
       </footer>
